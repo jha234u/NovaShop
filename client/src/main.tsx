8055d8ea471ffb28ae34e40e5e5c078be7b6143b
Divyanshi Jha <enemy124t@gmail.com>
2026-08-08 13:24:53 +0530

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,7 +5,7 @@ import { Toaster } from "react-hot-toast";
 import "./index.css";
 import App from "./App.jsx";
 
-import { AuthProvider } from "./context/AuthContext.jsx";
+import { AuthProvider } from "./context/AuthContext.tsx";
 import { CartProvider } from "./context/CartContext.jsx";
 
 createRoot(document.getElementById("root")).render(
