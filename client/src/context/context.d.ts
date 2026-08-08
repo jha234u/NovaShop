7ca8b11991e6104fe79b0a8220d4942fceb79bcf
Divyanshi Jha <enemy124t@gmail.com>
2026-08-08 17:38:48 +0530

    Delete client directory

--- client/src/context/context.d.ts
+++
@@ -1,9 +0,0 @@
-declare module './context/AuthContext' {
-  import { ReactNode } from 'react';
-  export const AuthProvider: React.FC<{children: ReactNode}>;
-}
-
-declare module './context/CartContext' {
-  import { ReactNode } from 'react';
-  export const CartProvider: React.FC<{children: ReactNode}>;
-}
