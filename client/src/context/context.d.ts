b066e3d89679572e40637ecaf1f9723037e75fcc
Divyanshi Jha <enemy124t@gmail.com>
2026-08-08 13:49:32 +0530

    Update context.d.ts

--- client/src/context/context.d.ts
+++ client/src/context/context.d.ts
@@ -1,1 +1,8 @@
+declare module './context/AuthContext.jsx' {
+  export const AuthProvider: React.FC<React.PropsWithChildren<{}>>;
+}
+
+declare module './context/CartContext.jsx' {
+  export const CartProvider: React.FC<React.PropsWithChildren<{}>>;
+}
 
