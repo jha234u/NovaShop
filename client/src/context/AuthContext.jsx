2d15d407e4c14192350ec1034ca944ee044b3c13
jha234u <enemy124t@gmail.com>
2026-08-08 13:28:54 +0530

    Update client and fix deployment

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Toaster } from "react-hot-toast";
-import { CartProvider } from "./context/CartContext.jsx";
-import { AuthProvider } from "./context/AuthContext.jsx";
 
 import "./index.css";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")!).render(
+import { AuthProvider } from "./context/AuthContext.jsx";
+import { CartProvider } from "./context/CartContext.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
       <CartProvider>
